4c993ce693eb008e7b2fabc14a8bdcd1b05b34a2
Marcelina <marcelinasiwecka@interia.pl>
2020-04-21 18:06:50 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const menuMobileBurger = document.querySelector(".burger-icon");
+const menuMobileClose = document.querySelector(".close-icon");
+const menuListMobile = document.querySelector(".main-nav");
+
+menuMobileBurger.addEventListener("click", () => {
+  menuListMobile.classList.toggle("mobile-show");
+
+  const imgSrc = menuMobileBurger.firstChild.src;
+
+  const imgPathShort = imgSrc.replace(/^.*[\\\/]/, "");
+
+  if (imgPathShort === `icon-hamburger.svg`) {
+    menuMobileBurger.firstChild.src = "images/icon-close.svg";
+  } else {
+    menuMobileBurger.firstChild.src = "images/icon-hamburger.svg";
+  }
+});
